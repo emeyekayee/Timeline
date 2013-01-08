b55170d1f14a16c46589b4aa084acff06b2b62f2
Mike Cannon <michael.j.cannon@gmail.com>
2013-01-08 11:55:03 -0800

    Updates to RDOC, client auto-update, specs.

--- tv/app/models/timeheader.rb
+++ tv/app/models/timeheader.rb
@@ -14,12 +14,25 @@ class Timeheader
     @@header_by_rid[@rid = rid] = self
   end
 
-  # For SchedResource
-  # Return Timeheader* object from resource id (string)
+  # (SchedResource protocol)  Returns the corresponding Timeheader object 
+  # given a resource id (string).  These rids (specified in schedule.yml) are
+  # pretty much arbitrary as long as they are unique for this resource class.
+  #
+  # ==== Parameters
+  # * <tt>rid</tt> - A (string) resource id.
+  #
+  # ==== Returns
+  # * <tt>Timeheader</tt> - A Timeheader corresponding to the given <tt>rid</tt>,
+  #   such as 'Hour0'.
   def self.find_as_schedule_resource( rid )
     @@header_by_rid[rid] || (@@header_by_rid[rid] = new(rid))
   end
 
+  # (For SchedResource protocol)  This method lets us set display attributes
+  # on the instance in a resource-class-specific way.
+  # 
+  # ==== Parameters
+  # * <tt>rsrc</tt> - A SchedResource instance. 
   def decorate_resource( rsrc )
     klass = SchedResource.block_class_for_resource_name(self.class.name)
     rsrc.label = klass.label
--- tv/app/models/timelabel.rb
+++ tv/app/models/timelabel.rb
@@ -16,7 +16,29 @@ class Timelabel
     @starttime, @endtime = [t, t+t_block].map{|t| t.to_i.to_s}
   end
 
-  # ScheduledResource protocol...
+  # (SchedResource protocol) Returns a hash where each key is an
+  # <tt>rid</tt> and the value is an array of Timelabels (use
+  # blocks) in the interval <tt>t1...t2</tt>, ordered by
+  # <tt>starttime</tt>.
+  #
+  # What <em>in</em> means depends on *inc*.  If inc(remental) is 
+  # false, client is building interval from scratch.  If "hi", it is
+  # an addition to an existing interval on the high side.  Similarly
+  # for "lo".  This is to avoid re-transmitting blocks that span the
+  # current time boundaries on the client.
+  #
+  # Here the resource is a channel and the use blocks are programs.
+  # 
+  # ==== Parameters
+  # * <tt>rids</tt> - A list of schedules resource ids (strings).
+  # * <tt>t1</tt>   - Start time.
+  # * <tt>t2</tt>   - End time.
+  # * <tt>inc</tt>  - One of nil, "lo", "hi" (See above).
+  #
+  # ==== Returns
+  # * <tt>Hash</tt> - Each key is a <tt>rid</tt> such as Hour0
+  # and the value is an array of Timelabels in the interval, ordered by
+  # <tt>starttime</tt>.
   def Timelabel.get_all_blocks(ids, t1, t2, inc)
     h = {}; ids.each{|id| h[id] = get_timeblocks(id, t1, t2, inc)}; h
   end
